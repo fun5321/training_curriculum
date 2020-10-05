f58d6279250f19f4f8ce068e07ab5760ca9dc6af
fun5321 <soffy831@gmail.com>
2020-10-05 16:51:50 +0900

    変数名を小文字に修正　コード字下げを修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,7 +3,7 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
-    @Plan = Plan.new
+    @plan = Plan.new
   end
 
   # 予定の保存
@@ -29,14 +29,14 @@ class CalendarsController < ApplicationController
 
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
-    7.times do |x|
-      today_plans = []
-      plan = plans.map do |plan|
-      today_plans.push(plan.plan) if plan.date == @todays_date + x
-      end
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
-      @week_days.push(days)
-    end
-
+  7.times do |x|
+    today_plans = []
+    plan = plans.map do |plan|
+    today_plans.push(plan.plan) if plan.date == @todays_date + x
   end
+    days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+    @week_days.push(days)
+  end
+
+ end
 end
